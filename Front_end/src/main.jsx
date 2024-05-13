07716bf277ccb1719eac051487262fb71b100318
Leonardo-Marcilino <leonardo.marcilino@hotmail.com>
2024-05-13 17:00:15 -0300

    Iniciação do react; organização de pastas; criação do reset e root

---
+++ Front_end/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './root.css'
+import './reset.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    aaaaaa
+  </React.StrictMode>
+)
